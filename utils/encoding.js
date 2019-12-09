ebc2555bf91b894426b0fe0d73becc9ce2ceb5c9
Zero <zero1729@protonmail.com>
2019-12-09 15:04:31 +0100

    Encoder expander/shortner fns

---
+++ utils/encoding.js
@@ -0,0 +1,45 @@
+const AbbvEnconding = (str) => {
+    if (str.slice(0, 4) == 'base') {
+        // so that 'base64' shortens to 'b64',
+        // ... 'base58' to 'b58', etc
+        return 'b' + str.slice(4)
+    } else {
+        return str
+    }
+}
+
+const ExpandEncoding = (str) => {
+    if (str[0] == 'b' && str.length == 3) {
+        // so that 'base64' shortens to 'b64',
+        // ... 'base58' to 'b58', etc
+        return 'base' + str.slice(1)
+    } else {
+        return str
+    }
+}
+
+
+const MakeBuffer = (hash, encoding, join=false) => {
+    // JS does not know what 'b64 is, so we expand it to 'base64'
+    encoding = encoding == 'b64' ? ExpandEncoding(encoding) : encoding
+
+    if (!['base64', 'hex'].includes(encoding)) {
+        throw new Error("[Encoding Error] Encoding must be either 'hex' or 'base64'")
+    }
+
+    // So we can just concatenate a long string of hashes
+    if (join) {
+        let hexed_string = ''
+
+        for (var i = 0;i < hash.length;i++) {
+            hexed_string += Buffer.isBuffer(hash[i]) ? hash[i].toString('hex') : hash[i]
+        }
+
+        return Buffer.from(hexed_string, encoding)
+    } else {
+        return Buffer.from(hash, encoding)
+    }
+}
+
+
+module.exports = { AbbvEnconding, ExpandEncoding, MakeBuffer }
