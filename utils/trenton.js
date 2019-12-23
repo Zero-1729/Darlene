7689c7e3de84926cb1a9971a110708c66e87395c
Zero <zero1729@protonmail.com>
2019-12-23 17:06:27 +0100

    Trenton Upgraded ⚡️
    - Abandoned scan and parse approach.
    - Trenton handles:
        - valid args: checks whether the argument provided for 'keylength', 'encoding', and 'mode' is valid.
        - required args: whether option's arg was provided.
        - unknown args: whether the option is valid/recognized.
    - Builds the metas for Darlene
    
    Note: Still need to add fn to recognize and report dependent options, e.g. 'ext' <-> 'file', 'file' <-> 'out'

--- utils/trenton.js
+++ utils/trenton.js
@@ -1,95 +1,214 @@
 /*
-* Trenton: A small commandline parser
+* Trenton: A small hacky commandline wrangler
 */
 
+const path = require('path')
 
-const alphas = 'abcdefghijklmnopqrstuvwxyz'
-const digits = '0123456789'
 
-const isAlpha = (str) => { return alphas.includes(str.toLowerCase()) }
-const isDigit = (str) => { return digits.includes(str.toLowerCase()) }
-const isAtEnd = (idx, lim) => { return idx >= lim }
+// Easily translate some options to metas key
+const meta_aliases = {
+    'J': 'isJSON',
+    'm': 'mode',
+    'k': 'keylength',
+    'e': 'encoding',
+    'c': 'content',
+    'f': 'file',
+    'o': 'out',
+    's': 'show'
+}
 
-const scan = (str) => {
-    let tokens = []
-    let index = 0
+// To manage valid option arguments
+valid_required_values = {
+    'keylength': ['128', '196', '256'],
+    'encoding': ['hex', 'base64'],
+    'mode': ['cbc', 'gcm']
+}
 
-    while (index < str.length) {
-        // Skip blank lines
-        if (str[index] == ' ') {
-            index += 1
+// All valid options and flags
+const valid_args = [ '-h',
+                    '--help',
+                    '-c', 
+                    '--content',
+                    '-f',
+                    '--file',
+                    '-o',
+                    '--out',
+                    '-m',
+                    '--mode',
+                    '-k',
+                    '--keylength',
+                    '-e',
+                    '--encoding',
+                    '-B',
+                    '--binary',
+                    '-J',
+                    '--json',
+                    '-S',
+                    '--show' ]
 
-            continue
-        } else {
-            // We expect arguments to be words or numbers
-            // E.g. '128', 'input.txt', etc
-            if (isAlpha(str[index]) || isDigit(str[index])) {
-                // We have hit an argument
-                let arg = ''
+// Our single character flags and their expanded versions
+const flags = ['B', 'binary', 'J', 'json', 'S', 'show']
 
-                // We build the argument letter by letter
-                // and digit by digit
-                while (!isAtEnd(index, str.length)) {
-                    if (isAlpha(str[index]) ||
-                        isDigit(str[index]) || str[index] == '.') {
-                        arg += str[index]
+const joinArray = (arr) => {
+    // Function returns a stringified array in the form '<elm>, <elm>, ... or <elm>'
+    if (arr.length > 1) {
+        let str = arr.slice(0, arr.length-1).join(', ')
 
-                        // Advacne index
-                        index += 1
-                    } else { break }
-                }
-
-                tokens.push(arg)
-            }
-
-            if (str[index] == '-') {
-                // Advance
-                index += 1
-
-                // We have hit an argument
-                let term = ''
-
-                // In case double dashes where used
-                // Perform length check first, to avoid index errors
-                if (!isAtEnd(index, str.length)) {
-                    if (str[index] == '-') {
-                        // if another dash found, we simply advance
-                        index += 1
-                    }
-                }
-
-                while (!isAtEnd(str[index])) {
-                    if (isAlpha(str[index])) {
-                        term += str[index]
-
-                        // advance the token
-                        index += 1
-                    } else {
-                        break
-                    }
-                }
-
-                tokens.push(term)
-            }
+        if (arr.length > 2) {
+            str += ' or ' + arr[arr.length - 1]
         }
 
-        index += 1
+        return str
+    } else {
+        return arr.toString()
     }
-
-    return tokens
 }
 
-const parse = (tokens) => {
-    // We assume that the tokens are of even length
-    // ... an equal bijection can be made
-    // ... in order to create a dict of metadata
-    let meta = {}
+const getValue = (args, idx) => {
+    // Returns the value of an argument if available
+    // ... else it throws an error
+    let raw_arg = stripArg(args[idx])
+    let arg = raw_arg.replace((new RegExp('-', 'g')), '') // Get stripped arg
 
-    for (var i = 0;i < tokens.length/2;i++) {
-        meta[tokens[i*2]] = tokens[i*2 + 1]
+    // If its a flag that doesn't require an argument then we simply return arg
+    if (flags.includes(arg)) {
+        let idx = flags.indexOf(arg)
+
+        // expand single flag name
+        // E.g. 'B' -> 'binary'
+        idx = (idx + 1 == flags.length) || (idx - 1 % 2 == 0) ? idx : idx + 1
+
+        // Remember including a flag sets the value to true
+        return {arg: flags[idx], value: true, inc: 0, ret_val: 0, invalid: false, valids: []}
     }
 
-    return meta
+    // expand single flag names
+    // E.g. 'f' -> 'file'
+    if (Object.keys(meta_aliases).includes(arg)) {
+        arg = arg.length == 1 ? meta_aliases[arg] : arg
+    }
+
+    // Check if the option carries an arg
+    let nidx = idx+1
+
+    if (nidx >= args.length) {
+        return {arg: raw_arg, value: `'<${arg}>'`, inc: null, ret_val: 1, invalid: false, valids: []}
+    }
+
+     // Check if arguments 'keylength', 'encoding', and 'mode' are valid
+     if (Object.keys(valid_required_values).includes(arg)) {
+        if (!valid_required_values[arg].includes(args[nidx])) {
+            return {arg: raw_arg, value: `'<${arg}>'`, inc: null, ret_val: 1, invalid: true, valids: valid_required_values[arg]}
+        }
+    }
+
+    return {arg: arg, value: args[nidx], inc: 1, ret_val: 0, invalid: false, valids: []}
 }
 
-module.exports = { scan, parse }
+const stripArg = (str) => {
+    let arg = str
+
+    // Watch out for potential use of flags with '='
+    // E.g. --content="Hello" or -c="Hello"
+    if (arg.match(/^-(-)?.+=/)) {
+        // extract the command
+        arg = arg.replace(/=.+/, '')
+    }
+
+    return arg
+}
+
+const stripValue = (str) => {
+    let val = str
+
+    if (str.match(/^-(-)?.+=/)) {
+        // Fetch value after '='
+        val = str.replace(/^-(-)?.+=/, '')
+    }
+
+    return val
+}
+
+const sanitizeArgs = (args) => {
+    let newArgs = []
+
+    for (var i = 0;i < args.length;i++) {
+        // Do we have '--<cmd>=val'
+        if (args[i].match(/^-(-)?.+=/)) {
+            // If so, we have to extract the arg from the value
+            newArgs.push(stripArg(args[i]))
+            newArgs.push(stripValue(args[i]))
+        } else {
+            // If not, most likely separated by newlines
+            // ... so we just return it the way it is
+            newArgs.push(args[i])
+        }
+    }
+
+    return newArgs
+}
+
+const buildMeta = (args) => {
+    // Default values for metas
+    let metas = {
+        mode: 'gcm',
+        keylength: 256,
+        encoding: 'hex',
+        isJSON: false,
+        ext: null,
+        file: null,
+        out: '.',
+        content: null,
+        show: false
+    }
+
+    let i = 0
+
+    // Main work done here
+    // Traversing the args and updating the metas accordingly
+    while (i < args.length) {
+        if (valid_args.includes(args[i])) {
+            let obj = getValue(args, i)
+
+            if (obj.ret_val == 0) {
+                // Special case for 'binary' flag
+                // Since it determins the file content type
+                // ... we have to set the 'ext' to true and then fill later
+                // ... this way, Darlene can handle the file contents properly when encrypting/decryprting
+                if (obj.arg == 'B' || obj.arg == 'binary') {
+                    metas['ext'] = true
+                } else {
+                    // Other metas are updating by direct indexing
+                    // ... since the 'arg' is expanded and matches the keys
+                    metas[obj.arg] = obj.value
+                }
+    
+                // Instead of a single increment
+                // We increment beyond the argument's value
+                i += (1 + obj.inc)
+            } else {
+                // Prints whether no value was provided for a flag or 
+                // whether an invalid value was provided with a certain flag
+                throw `darlene: flag '${stripArg(obj.arg)}' requires ${obj.invalid ? 'value of either' : 'an argument'} ${obj.invalid ? joinArray(obj.valids) : ''}`
+            }
+        } else {
+            // Alerts user that the option does not exist
+            throw `darlene: invalid option '${stripArg(args[i])}'`
+        }
+    }
+
+    // Check if dependent args provided
+    // can't provide 'ext' or isJSON flag if no '-f' file in
+    
+    // Set ext value
+    if (metas.ext) {
+        // Remember that the 'binary' option set this to true
+        // ... now we fill it in with the actual ext value
+        metas.ext = metas.file.slice(metas.file.lastIndexOf('.')+1)
+    }
+  
+    return metas
+
+}
+
+module.exports = { buildMeta, sanitizeArgs }
