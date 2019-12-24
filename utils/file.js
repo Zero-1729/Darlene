4fd5b4e3ca08fb4c8c03e26b6360750ca6f5aaac
Zero <zero1729@protonmail.com>
2019-12-25 00:11:58 +0100

    Fixed file writer fn
    - extension just appended to file path
    - file path of file written returned, so we don't have to calculate it ahead of time

--- utils/file.js
+++ utils/file.js
@@ -1,6 +1,7 @@
-const fs = require('fs')
-
 const { AbbvEnconding, ExpandEncoding, MakeBuffer } = require('./encoding')
+
+const fs   = require('fs')
+const path = require('path')
 
 
 const isEmptyBuffer = (buff) => {
@@ -140,18 +141,20 @@ const ReadFile = (fp, print=true) => {
     return content
 }
 
-const WriteFile = (fp, data, ext='drln') => {
+const WriteFile = (fp, data, ext='.drln') => {
     if (Buffer.isBuffer(ext)) {
         // Only attempt to sanitize if buffer
         ext = getExtension(ext)
     }
 
     let lastDotIdx = fp.lastIndexOf('.')
-    let outfp = lastDotIdx != -1 ? 
-                fp.slice(0, lastDotIdx > 0 ? lastDotIdx : 
-                fp.length ) + '.' + ext : fp + '.' + ext
+    let outfp = path.extname(fp) ? 
+                fp.slice(0, lastDotIdx) + ext : fp + ext
 
     fs.writeFileSync(outfp, data)
+
+    // So we can know the final file path written to
+    return outfp
 }
 
 module.exports = { ReadFile, WriteFile, CreateData, GetMeta, PrintContent }
